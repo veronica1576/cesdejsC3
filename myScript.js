fb7db6910a78180d4d18029bb4cd89febe66ce61
veronica1576 <desarrollo@academicos.cesde.edu.co>
2023-08-22 20:56:28 -0500

    java 01

---
+++ myScript.js
@@ -0,0 +1,64 @@
+//Generalidades
+
+//JS sabe que la variable es una cadena porque esta en "" o `` o en ''.
+
+
+let nombreParticipante = "Pedro"; 
+var deporte = 'Atletismo';
+const premio = `Oro`;
+
+var Mensaje = 'Este es un mensaje "Entre comillas", ojo'; //eL tipo de comilla depende del tipo de mensaje que quiero imprimir
+
+//Number - Números
+miNumero = 3;
+miOtroNumero = 3.5;
+
+//Boolean - Booleano
+miResultado = true;
+miOtroResultado = false;
+
+//Salidas en JS
+
+//alert("Mensaje de alerta para ver la salida");
+
+//edad= window.prompt("Digita tu edad:");
+
+//document.getElementById("deportista").innerHTML = edad;
+
+//document.write(miResultado);
+
+//console.log("Mensaje para la consola");
+
+//OPERADORES
+
+x = 3;
+y = 6;
+z = x + y;
+m = x - y;
+n= x * y;
+k= x / y;
+o= x % y;
+p= ++x; //sumar valor antigo con el actual
+t= --y; //Restar el valor antiguo con el actual
+a= Math.abs(x); //Valor absoluto
+
+document.write("<br>");
+document.write(x);
+document.write("<br>");
+document.write(y);
+document.write("<br>");
+document.write(z);
+document.write("<br>");
+document.write(m);
+document.write("<br>");
+document.write(n);
+document.write("<br>");
+document.write(k);
+document.write("<br>");
+document.write(o);
+document.write("<br>");
+document.write(p);
+document.write("<br>");
+document.write(t);
+document.write("<br>");
+document.write(a);
